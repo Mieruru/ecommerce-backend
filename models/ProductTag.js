12dece7da038fa7aa17e28a46047533adcda1e99
Cassidy Pagels <rozeneclipse@gmail.com>
2024-10-23 21:04:28 -0500

    add ProductTag and Tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,40 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      // integer
+      type: DataTypes.INTEGER,
+      // doesn't allow null values
+      allowNull: false,
+      // set as primary key
+      primaryKey: true,
+      // uses autoincrement
+      autoIncrement: true,
+    },
+
+    product_id: {
+      // integer
+      type: DataTypes.INTEGER,
+      // references the Product model's id
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      // integer
+      type: DataTypes.INTEGER,
+      // references the Tag model's id
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,25 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
     // define columns
+    id: {
+      // integer
+      type: DataTypes.INTEGER,
+      // doesn't allow null values
+      allowNull: false,
+      // set as primary key
+      primaryKey: true,
+      // uses autoincrement
+      autoIncrement: true,
+    },
+    tag_name: {
+      // string
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
