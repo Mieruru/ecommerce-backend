c4c57165c9817f99e01a031a8ba6cab7e1a7d956
Cassidy Pagels <rozeneclipse@gmail.com>
2024-10-23 20:57:09 -0500

    update .gitignore, add Category and Product models

--- models/Product.js
+++ models/Product.js
@@ -4,12 +4,53 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      // decimal
+      type: DataTypes.DECIMAL,
+      // doesn't allow null values
+      allowNull: false,
+      // validates that the value is a decimal
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      // integer
+      type: DataTypes.INTEGER,
+      // doesn't allow null values
+      allowNull: false,
+      // set a default value of 10
+      defaultValue: 10,
+      // validates that the value is numeric
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      // integer
+      type: DataTypes.INTEGER,
+      // references the Category model's id
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
